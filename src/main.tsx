7cbcee783e851077b2b7b095b77accfede29664c
young jo <a10019610@gmail.com>
2025-08-20 18:46:47 +0900

    chore: main.tsx 수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './routes/Router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthProvider } from './context/AuthContext';
 
 // Tanstack Query Setting
 const queryClient = new QueryClient();
@@ -10,7 +11,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <Router />
+      <AuthProvider>
+        <Router />
+      </AuthProvider>
     </BrowserRouter>
-  </QueryClientProvider>,
+  </QueryClientProvider>
 );
