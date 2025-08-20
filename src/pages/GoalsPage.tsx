d929b8546775fb0cf605d18c775790d49ad5e0b7
hjkim <20220330jin@gmail.com>
2025-08-20 16:43:14 +0900

    add: NavBar 작업

---
+++ src/pages/GoalsPage.tsx
@@ -0,0 +1,5 @@
+const GoalsPage = () => {
+  return <div>Goals Page</div>;
+};
+
+export default GoalsPage;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from '../layouts/Layout';
 import MainPage from '../pages/MainPage';
+import GoalsPage from '../pages/GoalsPage.tsx';
 
 const Router = () => {
     return (
         <Routes>
             <Route element={<Layout />}>
                 <Route path='/' element={<MainPage />} />
+                <Route path='/goals' element={<GoalsPage />} />
             </Route>
         </Routes>
     );
