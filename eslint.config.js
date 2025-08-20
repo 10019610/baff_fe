610f0e1da91f0d2cca0141e0afc17996cceec4eb
young jo <a10019610@gmail.com>
2025-08-20 15:49:12 +0900

    chore: tailwindCSS v4 설정 및 prettier, lint 설정중

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,37 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+/** @type {import('eslint').Linter.Config[]} */
+export default [
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
   },
-])
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: 'detect', // React 버전 자동 감지
+      },
+    },
+    rules: {
+      semi: 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'react/react-in-jsx-scope': 'off', // React 17+ 에서는 필요없음
+      'react/prop-types': 'off', // TypeScript 사용시 prop-types 불필요
+    },
+  },
+];
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,9 @@
 const MainPage = () => {
-    return (
-        <div>
-            <h1>MainPage</h1>
-        </div>
-    );
+  return (
+    <div className="p-8 bg-background text-foreground">
+      <h1 className="text-2xl font-bold">MainPage</h1>
+    </div>
+  );
 };
 
 export default MainPage;
