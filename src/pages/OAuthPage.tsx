a1a26ece7229ad4b9180831f2987e50efe884e48
hjkim <20220330jin@gmail.com>
2025-08-25 19:37:34 +0900

    feat: solve login problem

--- src/pages/OAuthPage.tsx
+++ src/pages/OAuthPage.tsx
@@ -1,29 +1,25 @@
-import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext.tsx';
 
 const OAuthPage = () => {
   const navigate = useNavigate();
-  const { token, expirationTime } = useParams();
   const { isAuthenticated, isLoading } = useAuth();
-  console.log('OAuthPage', token);
 
   useEffect(() => {
-    console.log('OAuthPage', token);
+    // AuthContext의 로딩 상태가 끝나면 로직을 실행합니다.
     if (!isLoading) {
       if (isAuthenticated) {
-        console.log('good');
+        // 인증 성공 시, 메인 페이지로 이동합니다.
         navigate('/');
       } else {
-        console.log('bad');
+        // 인증 실패 시, 에러 메시지와 함께 로그인 페이지로 이동합니다.
+        navigate('/login?error=auth_failed');
       }
     }
-    // if (token && expirationTime) {
-    //   navigate('/');
-    // }
   }, [isLoading, isAuthenticated, navigate]);
 
-  return <div>OAuth</div>;
+  return <div>로그인 처리 중...</div>;
 };
 
 export default OAuthPage;
