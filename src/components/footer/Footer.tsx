93876c29c34cdf948117858ee8c40e253afbb02a
young jo <a10019610@gmail.com>
2025-10-28 19:16:35 +0900

    feat: 패치 버전 수정

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import LumpenLogo from '../ui/LumpenLogo';
 import { useNavigate, Link } from 'react-router-dom';
 
 export default function Footer() {
-  const APP_VERSION = 'v1.3.1';
+  const APP_VERSION = 'v2.0.0';
   const navigate = useNavigate();
 
   const handleWithdraw = () => {
